23e2e4ae55da03fb8cea01aa2f5cd50ff7bb8957
Dario Jolo <dario.jolodovsky@enerminds.com>
2019-06-12 15:58:11 -0300

    Inicio del proyecto
    Inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:componentes/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Componentes App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes App'),
+        ),
+        body: Center(
+          child: HomePageTemp(),
+          ),
+        ),
+    );
+  }
+}
