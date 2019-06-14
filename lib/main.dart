35e88496cd6e34726f067137ef9923627f8c8e85
Dario Jolo <dario.jolodovsky@enerminds.com>
2019-06-14 18:05:28 -0300

    Se agrego la page home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
  
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      home: HomePageTemp(),
+      home: HomePage(),
           
         
     );
